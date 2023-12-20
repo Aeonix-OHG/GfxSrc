95f8b93e3c194777b6082b319e83dd9794f48a99
mail@jan-schmitt.eu <mail@jan-schmitt.eu>
2023-12-20 19:42:23 +0100

    Added one feture fixed 3 Bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
-use std::io;
+/*
+ * Copyright (c) 2023 Aeonix https://github.com/Aeonix-OHG
+ * All Rights Reserved
+ * Project: src
+ * File: lib.rs
+ * 
+ * Author: Jan Simon Schmitt
+ * Created: 11 12 2023
+ * Modified: 20 12 2023
+ * Modified By: Jan Simon Schmitt
+ */
+use std::io::{self, Write};
 
+// Implementing the Screen Structure
 pub struct Screen {
     width: usize,
     height: usize,
@@ -7,11 +19,14 @@ pub struct Screen {
     frame: Vec<Vec<char>>,
 }
 
+// Implementing the screen
 impl Screen {
+    // Creating a new Screen
     pub fn new(width: usize, height: usize, standartchar: char) -> Screen {
         let frame = vec![vec![standartchar; width]; height];
         Screen {width, height, standartchar, frame}
     }
+    // print out the screen
     pub fn print(&self) {
         for line in &self.frame {
             for character in line {
@@ -20,23 +35,27 @@ impl Screen {
             println!()
         }
     }
-    pub fn addinput(&mut self, x: usize, y: usize) -> String {
-        self.addstring(x, y, ">    ");
-        println!("{}{}", x, y );
+    // adds an input filed to the screen
+    pub fn addinput(&mut self, x: usize, y: usize, promt : String) -> String {
+        let xp = promt.len();
+        self.addstring(x, y, &promt);
         self.print();
+        print!("{}", promt);
+        io::stdout().flush().unwrap();
         let mut inputofusr = String::new();
         io::stdin()
             .read_line(&mut inputofusr)
             .expect("Error by reading input");
-        println!("{}{}", x + 1, y );
-        self.addstring(x + 1, y, &inputofusr.trim().to_string());
+        self.addstring(x + xp, y, &inputofusr.trim().to_string());
         self.print();
         inputofusr.trim().to_string()
     }
+    // clears the screen
     pub fn cls(&mut self) {
         self.frame = vec![vec![self.standartchar; self.width]; self.height];
         
     }
+    // adds a sting to the screen
     pub fn addstring(&mut self, x: usize, y: usize, text: &str) {
         if !(x + text.len() > self.width) && y <= self.height{
             let textlist = text.chars();
@@ -51,15 +70,17 @@ impl Screen {
     }
 }
 
+// Tests all funktions
+
 #[cfg(test)]
 mod tests {
     use crate::Screen;
 
     #[test]
     fn it_works() {
-        let mut app = Screen::new(10, 10, '#');
-        app.addstring(2, 2, "test");
-        let var1 = app.addinput(2, 3);
+        let mut app = Screen::new(30, 30, ' ');
+        app.addstring(2, 2, "123456");
+        let var1 = app.addinput(2, 3, "==> ".to_owned());
         app.addstring(2, 7, &var1);
         app.print();
     }
